d4cb8cbfb54dc1a6bae8e6f3ee5baa8378af58a3
Nicolas Espinoza <nico.dsgrafico@gmail.com>
2024-03-06 02:34:32 -0300

    :fire: Inicio de proyecto

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//Se crean 3 posibles variables que puede generar el math random.
+const piedra = 0;
+const papel = 1;
+const tijera = 2;
+
+var resultado = Math.floor(Math.random()*3)
+
+alert(resultado)
